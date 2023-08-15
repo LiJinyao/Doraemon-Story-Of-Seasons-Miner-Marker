a82c28f77272a5d36ebea96e8d0f355f40ddb100
kailous <kailous@live.cn>
2023-08-15 22:07:38 +0800

    添加了标记内容物的功能，点击图块，可以弹出的菜单选择标记的内容。

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
-# 大雄的牧场物语挖矿标记工具 Doraemon Story Of Seasons Miner Marker
+# 大雄的牧场物语挖矿标记工具
+Doraemon Story Of Seasons Miner Marker
 
 ![Screen Shot](screenshot.jpg?raw=true "Screen Shot")
+## 使用
+一个小工具，帮助你标记你想之后挖的矿的位置。<br>
+使用较新的Chrome打开 `index.html`. 单击添加标记，再次单击清除标记，点击Clean清除所有标记。<br>
 
-一个小工具，帮助你标记你想之后挖的矿的位置。
+## 更新
+添加了标记内容物的功能，点击图块，可以弹出的菜单选择标记的内容。<br>
+计划之后有时间添加层级的功能，虽然感觉可能用不上。<br>
 
+
+## Usage
 A simple tool helps you mark blocks you want to mine for latter.
-
-## 使用 Usage
-
-[在线使用 Online Tool](https://lijinyao.github.io/Doraemon-Story-Of-Seasons-Miner-Marker/)
-
-使用较新的Chrome打开 `index.html`. 单击添加标记，再次单击清除标记，点击Clean清除所有标记。
-
 Open `index.html` with a recently Chrome. Click to add a mark, click again cleans the mark. Clean button cleans all marks.
+Added the feature to mark content: Click on a block to open a menu and select the content for marking.
+
+## Update
+Added the feature to mark content: Click on a block to open a menu and select the content for marking.
+Plan to add the feature of layers, although I think it's not necessary.
+
 
 ## License
 
 MIT.
+
+[在线使用 Online Tool](https://kailous.github.io/Doraemon-Story-Of-Seasons-Miner-Marker/)
+
--- script.js
+++ script.js
@@ -1,87 +1,136 @@
 class App {
     constructor() {
-        let size = 7
-        let grid = new Array(size)
+        let size = 7; // 方块网格大小
+        let grid = new Array(size);
         for (let i = 0; i < size; i++) {
-            grid[i] = new Array(size)
+            grid[i] = new Array(size);
         }
-        this.size = size
-        this.grid = grid
+        this.size = size;
+        this.grid = grid; // 网格状态数组，用于存储标记信息
+        this.selectedBlock = null; // 当前选定的方块
     }
 
+    // 重置网格，清除类型内容和标记类
     resetGrid() {
-        document.querySelectorAll('.blockMarked').forEach(function (item) {
-            item.classList.remove('blockMarked')
-        })
-        for (let i = 0; i < this.size; i++) {
-            for (let j = 0; j < this.size; j++) {
-                this.grid[i][j] = 0
-            }
-        }
+        document.querySelectorAll('.block').forEach(function (item) {
+            const typeElement = item.querySelector('.type');
+            typeElement.textContent = ''; // 清空类型内容
+            item.classList.remove('blockMarked'); // 移除标记类
+        });
     }
 
+    // 添加选择器
     addSelector() {
-        const gridImg = document.querySelector(".grid")
-        let height = gridImg.height
-        let width = gridImg.width
-        console.log(height, width)
+        const gridImg = document.querySelector('.grid');
+        let height = gridImg.height;
+        let width = gridImg.width;
+        console.log(height, width);
     }
 
+    // 渲染方块网格
     renderkGrid() {
-        const map = document.querySelector(".gridContainer")
-        // 画一个正方形
-        const width = 110
-        const height = 110
-        const blockHeight = 55
-        const xscal = 0.805
-        const yscal = 0.405
-        //中心旋转的平移量
-        const dx = 159
-        const dy = 546
+        const map = document.querySelector('.gridContainer');
+        const width = 110; // 方块宽度
+        const height = 110; // 方块高度
+        const blockHeight = 55; // 方块半高度，用于定位中心点
+        const xscal = 0.805; // x轴缩放因子
+        const yscal = 0.405; // y轴缩放因子
+        const dx = 159; // x轴平移量
+        const dy = 546; // y轴平移量
+
         for (let i = 0; i < this.size; i++) {
             for (let j = 0; j < this.size; j++) {
-                // 齐次坐标变换
-                let x = width * i
-                let y = height * j
-                // 旋转+缩放变换，θ = 45° cos(θ) = 0.70710678
-                let nx = 0.70710678 * (x + y) * xscal + dx
-                let ny = 0.70710678 * (y - x) * yscal + dy
+                let x = width * i;
+                let y = height * j;
+                let nx = 0.70710678 * (x + y) * xscal + dx;
+                let ny = 0.70710678 * (y - x) * yscal + dy;
 
                 let block = document.createElement('div');
-                block.innerHTML = `<svg width="${width}px" height=${blockHeight}px>
-                <polyline class="blockBorder" points="3,${blockHeight / 2} ${width / 2},${blockHeight - 3} ${width - 3},${blockHeight / 2} ${width / 2},3 3,${blockHeight / 2}"/>
-                </svg>
-                <p class="coordinate">${i + 1},${j + 1}</p>
-                `
-                // let img = new Image(width, 42);
-                block.style.position = "absolute"
-                block.style.width = `${width}px`
-                block.style.height = `${blockHeight}px`
-                block.style.left = `${nx}px`
-                block.style.top = `${ny}px`
-                block.className = 'block'
-                map.append(block)
-                const handler = this.handlerBlockClick.bind(this)
-                block.addEventListener('click', function (params) {
-                    handler(this, i, j)
-                }, false)
+                block.innerHTML = `
+                    <svg width="${width}px" height=${blockHeight}px>
+                        <polyline class="blockBorder" points="3,${blockHeight / 2} ${width / 2},${blockHeight - 3} ${width - 3},${blockHeight / 2} ${width / 2},3 3,${blockHeight / 2}"/>
+                    </svg>
+                    <div class="info">
+                        <p class="coordinate">${i + 1}-${j + 1}</p>
+                        <p class="type"></p>
+                    </div>
+                `;
+
+                block.style.position = 'absolute';
+                block.style.width = `${width}px`;
+                block.style.height = `${blockHeight}px`;
+                block.style.left = `${nx}px`;
+                block.style.top = `${ny}px`;
+                block.className = 'block';
+                map.append(block);
             }
         }
     }
 
-    handlerBlockClick(target, x, y) {
-        target.classList.toggle('blockMarked')
-        console.log(x, y)
+    // 打开弹出菜单
+    openMenu(x, y) {
+        const menuOverlay = document.querySelector('.menu-overlay');
+        const menuContainer = document.querySelector('.menu-container');
+        menuContainer.style.left = `${x}px`;
+        menuContainer.style.top = `${y}px`;
+        menuOverlay.style.display = 'block';
     }
 
+    // 关闭弹出菜单
+    closeMenu() {
+        const menuOverlay = document.querySelector('.menu-overlay');
+        menuOverlay.style.display = 'none';
+    }
+
+    // 处理菜单项点击事件
+    handleMenuItemClick(value) {
+        if (this.selectedBlock) {
+            const typeElement = this.selectedBlock.querySelector('.type');
+            typeElement.textContent = `:${value}`; // 在 value 前添加 ":"
+            this.selectedBlock.classList.add('blockMarked'); // 添加标记类
+            this.closeMenu();
+        }
+    }
+
+    // 运行应用
     run() {
-        // mount reset
-        document.querySelector(".reset").addEventListener("click", this.resetGrid)
-        this.addSelector()
-        this.renderkGrid()
-        console.log("ready to go!")
+        document.querySelector('.reset').addEventListener('click', () => {
+            this.resetGrid();
+        });
+
+        this.addSelector();
+        this.renderkGrid();
+
+        const menuItems = document.querySelectorAll('.menu-item');
+        const blocks = document.querySelectorAll('.block');
+
+        // 监听菜单项点击事件
+        menuItems.forEach(item => {
+            item.addEventListener('click', () => {
+                this.handleMenuItemClick(item.getAttribute('data-value'));
+            });
+        });
+
+        // 监听方块点击事件
+        blocks.forEach(block => {
+            block.addEventListener('click', event => {
+                event.stopPropagation();
+                const x = event.clientX;
+                const y = event.clientY;
+                this.openMenu(x, y);
+                this.selectedBlock = block;
+            });
+        });
+
+        // 点击其他地方关闭菜单
+        document.addEventListener('click', () => {
+            this.closeMenu();
+        });
+
+        console.log('准备就绪！');
     }
 }
 
-app = new App()
-app.run()
+// 创建并运行应用
+const app = new App();
+app.run();
